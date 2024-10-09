f4299ce3658db11ad947eed60df38fa52b939874
TobTobXX <git@tobtobxx.net>
2024-10-09 11:17:30 +0200

    Fix server port

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def main():
 
 		refresh_button.click(fn=refresh_dropdowns, outputs=[rvc_dropdown, voice_dropdown])
 
-	interface.launch(server_name=os.environ['HOST'], server_port=os.environ['PORT'], quiet=True)
+	interface.launch(server_name=os.environ['HOST'], server_port=int(os.environ['PORT']), quiet=True)
 
 # delete later
 
